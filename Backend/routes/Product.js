5c27d23a4599c07577103f6bd16142a97021961e
shahil <shahilcode@gmail.com>
2023-04-25 15:09:18 +0530

    delete the product

--- Backend/routes/Product.js
+++ Backend/routes/Product.js
@@ -32,6 +32,15 @@ router.put('/update/:id', async (req, res) => {
     if (!updatedProduct) return res.status(404).send('Not updated user')
     res.send(updatedProduct)
 })
+// deleting product
+
+router.post('/delete/:id', async (req, res) => {
+    const id = req.params.id
+    const deleteOne = await productModel.deleteOne({ _id: new ObjectId(id) })
+    if (!deleteOne) return res.status(502).send("can't delete the product")
+    res.status(200).send(deleteOne)
+})
+
 
 
 module.exports = router
